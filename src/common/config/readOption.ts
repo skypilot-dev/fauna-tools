7848201d66cf16d9fb5789f6e2c641c26800dbd3
William Thorsen <william@thorsen.dev>
2020-02-13 03:57:42 -0700

    internal: Can read from an options file
    Added `readOption` function

---
+++ src/common/config/readOption.ts
@@ -0,0 +1,17 @@
+import * as path from 'path';
+import { findPackageFileDir } from '@skypilot/sugarbowl';
+import { getOrDefault } from '../object/getOrDefault';
+import { readOptionsFile } from './readOptionsFile';
+
+export function readOption<T>(objectPath: string): T | '' {
+  const options = readOptionsFile();
+  const localOptions = readOptionsFile({
+    pathToFile: path.resolve(findPackageFileDir(), 'local/fauna-tools.yml'),
+  });
+  const value = getOrDefault(
+    localOptions,
+    objectPath,
+    getOrDefault(options, objectPath)
+  );
+  return value === undefined ? '' : value;
+}
