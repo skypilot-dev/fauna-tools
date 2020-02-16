a082dd8155a35fdacb7f37038f03515108f74f39
William Thorsen <william@thorsen.dev>
2020-02-16 11:42:51 -0700

    fix: Deploy & migrate scripts can't execute subordinate script
    Fixed by calling the subordinate script via the package manager

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# Change to the directory of this script so that relative paths resolve correctly
-cd $(dirname "$0")
-
 if [[ $# -ne 1 ]]; then
   echo "Usage: $0 DB_NAME"
   exit 1
@@ -14,4 +11,14 @@ if [[ $1 == '--help' || $1 == '-h' || $1 == '-?' ]]; then
 fi
 
 DB_NAME=$1
-./upload-schema.sh ${DB_NAME} --override
+
+# This must be launched by a package manager that sets the `npm_config_user_agent` env var
+PACKAGE_MANAGER=$(echo "${npm_config_user_agent}" | awk -F '/' '{print $1}')
+COMMAND="fauna-upload-schema ${DB} --override"
+
+case $PACKAGE_MANAGER in
+  "npm") npx --no-install "${COMMAND}";;
+  "pnpm") pnpx --no-install "${COMMAND}";;
+  "yarn") yarn --silent "${COMMAND}";;
+  "*") echo "Unknown package mananger '${PACKAGE_MANAGER}'" ; exit 1;;
+esac
