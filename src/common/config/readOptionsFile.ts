fade59bfd96eff3dc370f01ec1574a0759a011fa
William Thorsen <william@thorsen.dev>
2021-04-01 12:44:32 -0700

    fix: Attempt to load YAML file fails after json-yaml package upgrade

--- src/common/config/readOptionsFile.ts
+++ src/common/config/readOptionsFile.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
-import yaml from 'js-yaml';
-import { findPackageFileDir } from '@skypilot/sugarbowl';
+import yaml, { JSON_SCHEMA } from 'js-yaml';
+import { findPackageFileDir, isObject } from '@skypilot/sugarbowl';
 
 interface ReadOptionsFileOptions {
   pathToFile?: string;
@@ -13,9 +13,9 @@ export function readOptionsFile(options: ReadOptionsFileOptions = {}): object {
     pathToFile = path.resolve(findPackageFileDir(), '.skypilot/fauna-tools.yaml'),
   } = options;
   if (fs.existsSync(pathToFile)) {
-    const fileContents = fs.readFileSync(pathToFile, { encoding: 'utf-8'} );
-    const parsedContents = yaml.safeLoad(fileContents);
-    if (typeof parsedContents === 'string') {
+    const fileContents = fs.readFileSync(pathToFile, { encoding: 'utf-8' } );
+    const parsedContents = yaml.load(fileContents, { schema: JSON_SCHEMA });
+    if (!isObject(parsedContents)) {
       throw new Error(`Unexpected non-object value read from YAML file: ${parsedContents}`);
     }
     return parsedContents || {};
